fe0d00a504cce970e321a508dc09af7797658168
Madhavendra Singh Shaktawat <madhavendrasinghshaktawat@gmail.com>
2023-09-09 13:58:32 +0530

    first commit

---
+++ d1p1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// using 1 cout
+	cout << "Hello world!!";
+	
+	// using to cout
+	cout << "\n";
+	cout << "hey xyz, \n";
+	cout << "this is to inform you that you are fucked up!!" << endl;
+	cout << "Hi there." ;
+	cout << "hii \\, \'";
+	
+	
+	// variables
+	int myNum = 5;               // Integer (whole number without decimals)
+	double myFloatNum = 5.99;    // Floating point number (with decimals)
+	char myLetter = 'D';         // Character
+	string myText = "Hello";     // String (text)
+	bool myBoolean = true;       // Boolean (true or false)
+	
+	cout << myNum << 
+	
+	return 0;
+	}
